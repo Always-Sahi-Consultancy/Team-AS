330182cd2e373ae8ccc8f6f318b600d6f19d2344
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-03-28 16:45:13 +0530

    AS Technologies Header made responsive with hamburger and colour chage on scroll

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
     $('.display-menu').on('click', function(){
-        $('.animated-icon').toggleClass('open');
+        $('.animated_icon').toggleClass('open');
     });
+
+    $(window).scroll(function(){
+        var scroll=$(window).scrollTop();
+        if(scroll>60){
+            $(".nav_bar").css("background", "#FFFBDF");
+            $('.menu').css('color', '#2B303A');
+            $('.content').css('color', '#2B303A');
+        }
+        else{
+            $(".nav_bar").css("background", "none");
+            $('.menu').css('color', '#FFFDBF');
+            $('.content').css('color', '#FFFBDF');
+        }
+    })
 });
