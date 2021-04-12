79c38585004494103035e2d93124825dfe2e6aed
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-04-12 22:36:35 +0530

    Always Sahi Main page footer updated and minor updates done.

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,35 @@ $(document).ready(function(){
         $('.animated_icon').toggleClass('open');
     });
 });
+
+$(document).ready(function(){
+    var state=0;
+    $('.a_atharv').on('click', function(){
+        if(state==0)
+        {
+            $('.atharv_info').css("display","block");
+            state=1;
+        }
+        else
+        {
+            $('.atharv_info').css("display","none");
+            state=0;
+        }
+    });
+});
+
+$(document).ready(function(){
+    var state=0;
+    $('.a_mandeep').on('click', function(){
+        if(state==0)
+        {
+            $('.mandeep_info').css("display","block");
+            state=1;
+        }
+        else
+        {
+            $('.mandeep_info').css("display","none");
+            state=0;
+        }
+    });
+});
