247091915176f53df3bd83f56b0143d85c7e9ca3
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-04-17 22:14:10 +0530

    AS Main Founders Note updated. Technology Footer.

--- js/menu.js
+++ js/menu.js
@@ -10,11 +10,13 @@ $(document).ready(function(){
         if(state==0)
         {
             $('.atharv_info').css("display","block");
+            $('.a_atharv').css("position","static");
             state=1;
         }
         else
         {
             $('.atharv_info').css("display","none");
+            $('.a_atharv').css("position","relative");
             state=0;
         }
     });
@@ -26,11 +28,13 @@ $(document).ready(function(){
         if(state==0)
         {
             $('.mandeep_info').css("display","block");
+            $('.a_mandeep').css("position","static");
             state=1;
         }
         else
         {
             $('.mandeep_info').css("display","none");
+            $('.a_mandeep').css("position","relative");
             state=0;
         }
     });
