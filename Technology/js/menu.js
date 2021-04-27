53569a244589548734c6870f7cec427ef28fa789
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-04-27 14:07:49 +0530

    Slider fixed external

--- Technology/js/menu.js
+++ Technology/js/menu.js
@@ -1,3 +1,33 @@
+
+var slideIndex = 1;
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+if (n > slides.length) {
+slideIndex = 1
+}    
+if (n < 1) {
+slideIndex = slides.length;
+}
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";  
+}
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " active";
+}
+
 $(document).ready(function(){
     var state=1;
     var scroll;
@@ -167,4 +197,13 @@ $(document).ready(function(){
         }
     });
 
+    
+    showSlides(slideIndex);
+
+    
+    
+
 });
+
+window.addEventListener('prev', plusSlides);
+window.addEventListener('next',plusSlides);
