48f4e2fec52d2a6d3d3298313337475a1f218754
MandeepDalavi <mandeepdalavi@gmail.com>
2021-05-01 21:22:44 +0530

    Made Main Page file's Code Prettier

--- js/menu.js
+++ js/menu.js
@@ -1,41 +1,35 @@
-$(document).ready(function(){
-    $('.display-menu').on('click', function(){
-        $('.animated_icon').toggleClass('open');
-    });
+$(document).ready(function () {
+  $(".display-menu").on("click", function () {
+    $(".animated_icon").toggleClass("open");
+  });
 });
 
-$(document).ready(function(){
-    var state=0;
-    $('.a_atharv').on('click', function(){
-        if(state==0)
-        {
-            $('.atharv_info').css("display","block");
-            $('.a_atharv').css("position","static");
-            state=1;
-        }
-        else
-        {
-            $('.atharv_info').css("display","none");
-            $('.a_atharv').css("position","relative");
-            state=0;
-        }
-    });
+$(document).ready(function () {
+  var state = 0;
+  $(".a_atharv").on("click", function () {
+    if (state == 0) {
+      $(".atharv_info").css("display", "block");
+      $(".a_atharv").css("position", "static");
+      state = 1;
+    } else {
+      $(".atharv_info").css("display", "none");
+      $(".a_atharv").css("position", "relative");
+      state = 0;
+    }
+  });
 });
 
-$(document).ready(function(){
-    var state=0;
-    $('.a_mandeep').on('click', function(){
-        if(state==0)
-        {
-            $('.mandeep_info').css("display","block");
-            $('.a_mandeep').css("position","static");
-            state=1;
-        }
-        else
-        {
-            $('.mandeep_info').css("display","none");
-            $('.a_mandeep').css("position","relative");
-            state=0;
-        }
-    });
+$(document).ready(function () {
+  var state = 0;
+  $(".a_mandeep").on("click", function () {
+    if (state == 0) {
+      $(".mandeep_info").css("display", "block");
+      $(".a_mandeep").css("position", "static");
+      state = 1;
+    } else {
+      $(".mandeep_info").css("display", "none");
+      $(".a_mandeep").css("position", "relative");
+      state = 0;
+    }
+  });
 });
