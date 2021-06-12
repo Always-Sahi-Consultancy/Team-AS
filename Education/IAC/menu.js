82e0c50a8b1f6e650960810274e58bb93549eef1
anjaliskalange <anjaliskalange@gmail.com>
2021-06-12 17:26:49 +0530

    iac

---
+++ Education/IAC/menu.js
@@ -0,0 +1,12 @@
+function Register(){
+    var a = document.getElementById("RegistrationForm")
+    var b = document.getElementById("Dashboard")
+    a.style.width="100%";
+    a.style.display="block";
+    b.style.display="none";
+}
+function dashboard(){
+    var x = document.getElementById("Dashboard")
+    x.style.width = "100%";
+    x.style.display = "block";
+}
