31c5951c71c900849d0783b7cac2fa443615154e
anjaliskalange <anjaliskalange@gmail.com>
2021-06-24 03:04:52 +0530

    IAC model js

--- Education/IAC/menu.js
+++ Education/IAC/menu.js
@@ -1,45 +1,65 @@
-function Register(){
-    var a = document.getElementById("RegistrationForm")
-    var iac=document.getElementById("IACRegistrationForm")
-    var b = document.getElementById("Dashboard")
-    a.style.width="100%";
-    a.style.display="block";
-    iac.style.display= "block";
-    b.style.display="none";
+function tabs(n){
+    var a = document.getElementById("ASSRegistrationForm");
+    var iac=document.getElementById("IACRegistrationForm");
+    var db = document.getElementById("Dashboard");
+    var l= document.getElementById("verifications");
+    var mC =document.getElementById("MyCourses");
+    var rf=document.getElementById("ReferalLink");
+    var supports= document.getElementById("generateticket");
+
+    if(n==1)
+    {
+        db.style.display="block";
+    }
+    if(n==2)
+    {
+        mC.style.display="block";
+        db.style.display="none";
+    }
+    if(n==3)
+    {
+        rf.style.display="block";
+        db.style.display="none";
+    }
+    else if(n==4)
+    {
+        a.style.display="block";
+        iac.style.display="block";
+        db.style.display="none";
+    }
+   else if(n==5)
+    {
+        mr.style.display="block";
+        db.style.display="none";
+    }
+    else if(n==6)
+    {
+        requests.style.display="block";
+        db.style.display="none";
+    }
+    else if(n==7)
+    {
+        supports.style.display="block";
+        db.style.display="none";
+    }
+    else if(n==8)
+    {
+        l.style.display="block";
+        db.style.display="none";
+    }
+
 }
-function dashboard(){
-    var x = document.getElementById("Dashboard")
-    x.style.width = "100%";
-    x.style.display = "block";
+window.addEventListener(onclick,tabs);
+
+function downline(){
+    document.getElementById("downlineInfo").style.display="block";
 }
 
-function IAC(){
-    var c = document.getElementById("IAC-business")
-    var d = document.getElementById("AssociateBusiness")
-    c.style.width= "100%";
-    c.style.display = "block";
-    d.style.display = "block";
+function iac(){
+    document.getElementById("IAC-business").style.display="block";
 }
 
-function KYC(){
-    var dash =document.getElementById("Dashboard")
-    var l= document.getElementById("verifications")
-    l.style.display = "block";
-    dash.style.display="none";
-    l.style.visibility="visible";
+function OTP(){
+    document.getElementById("OTP").style.display="block";
 }
 
-function MyCourse(){
-    var db= document.getElementById("Dashboard")
-    const mC =document.getElementById("MyCourses")
-    mC.style.width= "100%";
-    mC.style.display="block";
-    db.style.display= "none";
-}
-
-function  ReferalLink(){
-    var D = document.getElementById("Dashboard")
-    var rf=document.getElementById("ReferalLink")
-    rf.style.display= "block";
-    D.style.display="none";
-}
