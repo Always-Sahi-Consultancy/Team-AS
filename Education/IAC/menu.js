d9f1c145ba805d9f2292e7f78c347c845f3fbbab
anjaliskalange <anjaliskalange@gmail.com>
2021-06-25 02:09:37 +0530

    IAC finalization

--- Education/IAC/menu.js
+++ Education/IAC/menu.js
@@ -6,12 +6,14 @@ function tabs(n){
     var mC =document.getElementById("MyCourses");
     var rf=document.getElementById("ReferalLink");
     var supports= document.getElementById("generateticket");
+    var requests=document.getElementById("requests");
     a.style.display="none";
     iac.style.display="none";
     l.style.display="none";
     mC.style.display="none";
     rf.style.display="none";
     supports.style.display="none";
+    requests.style.display="none";
 
     if(n==1)
     {
@@ -71,9 +73,35 @@ function downline(){
 
 function iac(){
     document.getElementById("IAC-business").style.display="block";
+    document.getElementById("Dashboard").style.height="900px";
 }
 
-function OTP(){
-    document.getElementById("OTP").style.display="block";
+function OTP(n){
+    if(n==1)
+    {
+    document.getElementById("OTP1").style.display="block";
+    }
+    else if(n==2)
+    {
+    document.getElementById("OTP2").style.display="block";
+    document.getElementById("MobileChange").style.display="block";
+    document.getElementById("BO2").style.display="none";
+    }
+    else if(n==3)
+    {
+        document.getElementById("OTP3").style.display="block";
+        document.getElementById("BO3").style.display="none";
+    }
+    else if(n==4)
+    {
+        document.getElementById("OTP4").style.display="block";
+        document.getElementById("BO4").style.display="none";   
+    }
 }
+
+window.addEventListener(onclick,OTP);
+
+
+
+
 
