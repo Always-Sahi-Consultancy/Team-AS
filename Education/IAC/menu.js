140bdcc509ed6fc48af8f80f26f10a073fbbf5ac
anjaliskalange <anjaliskalange@gmail.com>
2021-06-28 02:13:48 +0530

    minor changes

--- Education/IAC/menu.js
+++ Education/IAC/menu.js
@@ -106,6 +106,20 @@ function OTP(n){
 
 window.addEventListener(onclick,OTP);
 
+$(function(){
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $(window).resize(function(e) {
+      if($(window).width()<=768){
+        $("#wrapper").removeClass("toggled");
+      }else{
+        $("#wrapper").addClass("toggled");
+      }
+    });
+  });
 
 
 
