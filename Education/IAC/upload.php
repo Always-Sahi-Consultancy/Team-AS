9fa2af8eb13feb7d5a3149053ee3c0c8555cfc56
MandeepDalavi <mandeepdalavi@gmail.com>
2021-07-09 00:37:03 +0530

    Added Backend function to upload files

---
+++ Education/IAC/upload.php
@@ -0,0 +1,37 @@
+<?php
+
+$fileF = $_SESSION['file'];
+
+$file = $_FILES[$fileF];
+$fileName = $_FILES[$fileF]['name'];
+$fileTmpName = $_FILES[$fileF]['tmp_name'];
+$fileSize = $_FILES[$fileF]['size'];
+$fileError = $_FILES[$fileF]['error'];
+$fileType = $_FILES[$fileF]['type'];
+$email = $_SESSION['email'];
+$id = $_SESSION['fileID'];
+
+$fileExt = explode('.', $fileName);
+$fileActualExt = strtolower(end($fileExt));
+
+$allowed = array('jpg', 'jpeg', 'png', 'pdf');
+
+if (in_array($fileActualExt, $allowed)) {
+    if ($fileError === 0) {
+        if ($fileSize < 204800) {
+            $fileNameNew = $email.".".$id.".".$fileActualExt;
+            $fileDestination = 'uploads/'.$fileNameNew;
+            move_uploaded_file($fileTmpName, $fileDestination);
+            echo '<script>alert("Files uploaded successfully    !")</script>';
+        }
+        else {
+            echo '<script>alert("Your file is too Big!")</script>';
+        }
+    }
+    else {
+        echo '<script>alert("There was an error uploading your file!")</script>';
+    }
+}
+else {
+    echo '<script>alert("You can not upload files of this type!")</script>';
+}
