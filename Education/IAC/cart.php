d9f2c23a4bf5b02645ba83a28d48c82a247ce830
MandeepDalavi <mandeepdalavi@gmail.com>
2021-07-19 23:42:43 +0530

    Updated Course View based on Category

--- Education/IAC/cart.php
+++ Education/IAC/cart.php
@@ -11,6 +11,16 @@
       foreach ($_SESSION['cart'] as $key => $value) {
         if($value["course_id"] == $_GET['id']) {
           unset($_SESSION['cart'][$key]);
+          $courseID = array_column($_SESSION['cart'], 'course_id');
+          while ($data = $result->fetch_assoc()) {
+            foreach ($courseID as $ID){
+              if ($data['id'] == $ID) {
+                cartEl($data['course_image'], $data['course_name'], $data['course_price'], $data['id']);
+                $total = $total - (float)$data['course_price'];
+                $_SESSION['price'] = $total;
+              }
+            }
+          }
           echo "<script>alert('Course has been removed!');</script>";
           echo "<script>window.location = 'cart.php'</script>";
         }
@@ -42,7 +52,7 @@
   <title>Courses | AS</title>
 </head>
 
-<body style="background-image: linear-gradient(140deg,#29648a 20%,#2e9cca,#aaabb8);">
+<body style="background-image: linear-gradient(140deg,#29648a 20%,#2e9cca,#aaabb8); background-repeat: no-repeat; background-size: 100% 150%;">
   <nav class="navbar navbar-expand-lg navbar-dark" style="background-image: linear-gradient(140deg,#29648a 20%,#2e9cca,#aaabb8);">
     <a href="course.php" class="navbar-brand">
       <h3 class="px-5">
@@ -94,6 +104,9 @@
             } else {
               echo "<h5>Cart is Empty</h5>";
             }
+            if ($total == 0) {
+              echo "<h5>Cart is Empty</h5>";
+            }
           ?>
         </div>
       </div>
@@ -128,7 +141,7 @@
             </div>
             <input type="hidden" name="price" value="<?php echo $total?>">
             <div class="input-group center">
-              <button type="submit" class="btn btn-warning pl-5 pr-5 mb-3" name="payBtn"><b>Pay</b></button>
+            <button type="submit" id="payButton" class="btn btn-warning pl-5 pr-5 mb-3" name="payBtn" <?php if ($total == '0'){ ?> disabled style='cursor:no-drop'<?php } ?> ><b>Pay</b></button>
             </div>
           </form>
         </div>
@@ -148,7 +161,7 @@
             </div>
             <div class=\"col-md-6\">
               <h5 class=\"pt-2\">$courseName</h5>
-              <small class=\"text-secondary\">Seller: dailytuition</small>
+              <!--<small class=\"text-secondary\">Seller: dailytuition</small>-->
               <h5 class=\"pt-2\">$coursePrice INR</h5>
               <button type=\"submit\" class=\"btn btn-danger\" name=\"remove\">Remove</button>
             </div>
@@ -160,7 +173,7 @@
       echo $element;
     }
   ?>
-
+  
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
