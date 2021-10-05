784803fe19c2d0661c1f2bd31d88b7f0b1dbd88a
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-10-05 12:22:20 +0530

    Header and Footer created and linked.

---
+++ footer.php
@@ -0,0 +1,194 @@
+<div class="footer container-fluid" id="footer">
+        <div class="footer-top row">
+            <div class=" col-xs-12 col-sm-6 col-lg-3">
+                <div class="feed-widget">
+                    <h2 class="f-head">OUR SERVICES</h2>
+                    <div class="footercarosule">
+                        <div class="item f-fade">
+                            <p>A series of training program designed, after years of experience and
+                                research, to create a pathway for every individual to achieve "A Package
+                                Escape".
+                            </p>
+                            <div class="icon"><i aria-hidden="true"><img src="image/3as.png" alt="logo" /></i>
+                                <h3><a href="Services">Package Escape Funnel</a></h3>
+
+                            </div>
+                        </div>
+                        <div class="item f-fade">
+                            <p>An Initiative introduced to, bring in more Investors as Well as Traders,
+                                into the world of Securities market. Providing them the Financial
+                                Freedom, and Future Planning.
+                            </p>
+                            <div class="icon"><i aria-hidden="true"><img src="image/4as.png" alt="logo" /></i>
+                                <h3><a href="MStake">MStake Funnel</a></h3>
+
+                            </div>
+                        </div>
+                        <div class="item f-fade">
+                            <p>Skill up yourself, with our Short workshops. Short in tenure, Large in
+                                Impact. To develop & Nourish skills in your field, we have designed quality
+                                based sessions, delivered by Experts.
+                            </p>
+                            <div class="icon"><i aria-hidden="true"><img src="image/5as.png" alt="logo" /></i>
+                                <h3><a href="workshops">Workshop</a></h3>
+                            </div>
+                        </div>
+                    </div><br>
+                    <div style="text-align:center">
+                        <span class="f-dot"></span>
+                        <span class="f-dot"></span>
+                        <span class="f-dot"></span>
+                    </div>
+                </div>
+            </div>
+            <div class=" col-xs-12 col-sm-6 col-lg-3">
+                <div class="feed-widget">
+                    <h2 class="f-head">JOIN US</h2>
+                    <div class="footercarosule">
+                        <div class="f-item ff-fade">
+
+                            <div class="icon"><i aria-hidden="true"><img src="image/join us/associate.jpg"
+                                        alt="logo" /></i>
+                                <a href="Services">Associate With Us</a>
+
+                            </div>
+                        </div>
+                        <div class="f-item ff-fade">
+
+                            <div class="icon"><i aria-hidden="true"><img src="image/join us/iasc.jpg" alt="logo" /></i>
+                                <a href="MStake">Be Our IAC</a>
+
+                            </div>
+                        </div>
+                        <div class="f-item ff-fade">
+
+                            <div class="icon"><i aria-hidden="true"><img src="image/join us/intern.jpg"
+                                        alt="logo" /></i>
+                                <a href="workshops">Career With AS</a>
+
+                            </div>
+                        </div>
+                    </div><br>
+                    <div style="text-align:center">
+                        <span class="ff-dot"></span>
+                        <span class="ff-dot"></span>
+                        <span class="ff-dot"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="f-team col-xs-12 col-sm-12 col-lg-6">
+                <div class="f-head ">OUR TEAM</div>
+                <div class="row">
+                    <div class="ft-img col-xs-12 col-sm-6 col-lg-4 thumbnail">
+                        <center><img src="image/team/atharv.png" alt="Atharv">
+                            <p>Atharv Sawant</p>
+                            <p>Founder & Ceo</p>
+                        </center>
+                    </div>
+                    </center>
+                    <div class="ft-img col-xs-12 col-sm-6 col-lg-4 thumbnail">
+                        <center><img src="image/team/mandy.png" alt="Mandy">
+                            <p>Mandeep Dalavi</p>
+                            <p>Co founder & Director</p>
+                        </center>
+                    </div>
+                    <div class="ft-img col-xs-12 col-sm-6 col-lg-4 thumbnail">
+                        <center><img src="image/team/sanika.png" alt="sanika">
+                            <p>Sanika Dewoolkar</p>
+                            <p>Chief Marketing Head</p>
+                        </center>
+                    </div>
+                    <div class="ft-img col-xs-12 col-sm-6 col-lg-4 thumbnail">
+                        <center><img src="image/team/akhil.png" alt="Akhil">
+                            <p>Akhil A Nair</p>
+                            <p>Chief Technical Head</p>
+                        </center>
+                    </div>
+                    <div class="ft-img col-xs-12 col-sm-6 col-lg-4 thumbnail">
+                        <center><img src="image/team/anjali.png" alt="Anjali">
+                            <p>Anjali Kalange</p>
+                            <p>Chief Training Head</p>
+                        </center>
+                    </div>
+                    <div class="ft-img col-xs-12 col-sm-6 col-lg-4 thumbnail">
+                        <center><img src="image/team/sanket.png" alt="Sanket">
+                            <p>Sanket Shinde</p>
+                            <p>Chief Associates Head</p>
+                        </center>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="footer-bottom row">
+            <div class="f-com col-xs-12 col-md-6 col-lg-3">
+                <div class="f-head">COMPANY</div>
+                <a href="#about_us">
+                    <p>About Us</p>
+                </a>
+                <a href="#workshop">
+                    <p>Services</p>
+                </a>
+                <a href="#">
+                    <p>Join Us</p>
+                </a>
+            </div>
+            <div class="f-add col-xs-12 col-md-6 col-lg-3">
+                <div class="f-head">ADDRESS</div>
+                <p><span>Address: </span>A-702,Aurum Vrundavan Apartment,
+                    Dighi, Pune, Maharashtra</p>
+                <p><span>Pincode: </span>411015</p>
+
+            </div>
+            <div class="f-con col-xs-12 col-md-6 col-lg-3">
+                <div class="f-head">CONTACT</div>
+                <a href="mailto:hello@alwayssahi.com"><span>Mail: </span>
+                    <p>hello@alwayssahi.com</p>
+                </a>
+                <a href="#"><span>Phone:</span>
+                    <p>+91 8857086790</p>
+                </a>
+
+            </div>
+            <div class="f-social col-xs-12 col-md-6 col-lg-3">
+                <div class="f-head">SOCIALS</div>
+                <div class="row">
+                    <div class="ft-icon col-xs-3">
+                        <center><a href="https://www.instagram.com/alwayssahi"><img src="image/icon/007-instagram-1.png"
+                                    alt="instagram"></a></center>
+                    </div>
+                    <div class="ft-icon col-xs-3">
+                        <center><a
+                                href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2FAlways-Sahi-Solutions-Private-Limited-100805488788703"><img
+                                    src="image/icon/002-facebook-1.png" alt="facebook"></a></center>
+                    </div>
+                    <div class="ft-icon col-xs-3">
+                        <center><a
+                                href="https://www.linkedin.com/company/always-sahi-solutions-private-limited/mycompany/?viewAsMember=true"><img
+                                    src="image/icon/002-linkedin-1.png" alt="linkedin"></a></center>
+                    </div>
+                    <div class="ft-icon col-xs-3">
+                        <center><a href="https://t.me/joinalwayssahi"><img src="image/icon/telegram (1).png"
+                                    alt="telegram"></a></center>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="ft-head row">
+                <div class="col-md-7 leftT">
+                    <div>@2021 Always Sahi Solutions Pvt Ltd: All Rights Reserved |
+                        ISO 9001:2015 Certified & MCA Registered
+                    </div>
+                </div>
+                <div class="ft-head-right col-md-5">
+                    <div class="row justify-content-center">
+                        <div><a href="Terms" class="Terms">Terms of Use </a>|&nbsp</div>
+                        <div ><a href="Privacy" class="Privacy">Privacy Policy </a>|&nbsp</div>
+                        <div ><a href="Refund" class="Refund">Refund Policy </a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
