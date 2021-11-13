9b5a9f08e1a456393483af662763314f3b5b11dc
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-11-13 17:35:10 +0530

    AS Site

---
+++ New Site/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/root.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/home.css">
+    <title>Always Sahi Academy</title>
+</head>
+<body>
+    <!-- This will show the currently whats going on in company -->
+    <div class="instant_focus">
+        Dhanashree ko Maro Pakad ke 
+    </div>
+    <!-- Header Section -->
+    <?php
+        include './public/header.php';
+    ?>
+
+    <!-- Main Page starts -->
+    <div class="intro">
+        <div class="intro__details">
+            <div class="intro__company_title">
+                <div class="intro__company_name">ALWAYS SAHI</div>
+                <div class="intro__company_section">Academy</div>
+            </div>
+            <div class="intro__paragraphs">
+                <div class="intro__paragraph_1">A Platform that believes in creating new Leaders, that make the world a beautiful place to Live.</div>
+                <div class="intro__paragraph_2">Be a Member of the “ASA Family” now from anywhere, just with a Laptop & a SmartPhone, & be the next leader in your field.</div>
+            </div>
+        </div>
+        <div class="intro__image">
+            <img src="image/Atharv_Sawant_Transparent.png" alt="Atharv Sawant">
+        </div>
+    </div>
+</body>
+</html>
---
+++ New Site/public/header.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="brand">
+        <img src="image/AS_Logo_Transparent.png" alt="AS Logo" class="brand__image">
+    </div>
+    <div class="hamburger">
+        <div class="hamburger__bar"></div>
+        <div class="hamburger__bar"></div>
+        <div class="hamburger__bar"></div>
+    </div>
+    <div class="navigate"> 
+        <div class="navigation_section">
+            <div class="navigate__link">Home</div>
+            <div class="navigate__link">ASA Story</div>
+            <div class="navigate__link">Testimonials</div>
+            <div class="navigate__link">ENroll Now</div>
+            <div class="navigate__link">Blogs</div>
+            <div class="navigate__link">Contact Us</div>
+            <div class="navigate__link">STudent Login</div>
+        </div>
+    </div>
+</header>
