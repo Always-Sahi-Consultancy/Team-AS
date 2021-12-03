30508bad7790d2d74f33af6c471f3398300fe259
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-12-03 15:03:21 +0530

    contactUs and ourStory

---
+++ New Site/contactUs.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/root.css" rel="stylesheet">
+    <link href="css/header.css" rel="stylesheet">
+    <link href="css/contactUs.css" rel="stylesheet">
+    <link href="css/contactUs_mobile.css" rel="stylesheet">
+    <link href="css/contactUs_tab.css" rel="stylesheet">
+    <title>Always Sahi Academy | Contact Us</title>
+</head>
+<body>
+    <?php 
+        include './public/header.php'
+    ?>
+    <div class="contact">
+        <form action="post" class="contact__form">
+            <input type="text" placeholder="Name" require>
+            <input type="email" >
+        </form>
+    </div>
+</body>
+</html>
