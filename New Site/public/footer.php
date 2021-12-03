0270866d70710f8ff2d482215810c38fd2f260a3
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-12-03 17:42:17 +0530

    footer modification

--- New Site/public/footer.php
+++ New Site/public/footer.php
@@ -3,10 +3,10 @@
         ASA Disclaimer:- Any understanding of the specific course will be the liability of the reader & the Individual itself, Organization does not hold any liability for comprehension of any Course, Module or any specific word in a wrong way. In case of any discrepancy, the interpretation of the academy will be deemed as final and absolute.
     </div>
     <div class="documentation">
-        <span class="documentation_tandc"><a href="#" class="documentation_link">Terms of use</a></span>
-        <span class="documentation_earning"><a href="#" class="documentation_link">Earning Disclaimer</a></span>
-        <span class="documentation_privacy"><a href="#" class="documentation_link">Privacy Policy</a></span>
-        <span class="documentaion_refund"><a href="#" class="documentation_link">Refund Policy</a></span>
+        <span class="documentation__option"><a href="#" class="documentation__link">Terms of use</a></span>
+        <span class="documentation__option"><a href="#" class="documentation__link">Earning Disclaimer</a></span>
+        <span class="documentation__option"><a href="#" class="documentation__link">Privacy Policy</a></span>
+        <span class="documentation__option"><a href="#" class="documentation__link">Refund Policy</a></span>
     </div>
     <div class="copyright">&copy; Always Sahi Solution Pvt. Ltd. <script>document.write(getFullYear())</script> - All Rights Reserved | &copy; ALL RIGHTS RESERVED</div>
 </footer>
